a36cab62388d03b798ce32c29ababdde6a4e3ed8
iamhernanflores <iamhernanflores@gmail.com>
2023-01-17 00:06:04 -0300

    add-code-vite

--- main.js
+++ main.js
@@ -2,6 +2,23 @@ import './style.css'
 import javascriptLogo from './javascript.svg'
 import { setupCounter } from './counter.js'
 
+import buttonStyle from './button.module.css'
+import imagenStyle from './image.module.css'
+import img from './vite-logo.png'
+import data from './data.json'
+
+import suma from './suma.ts'
+console.log(`suma 2+3 = ${suma(2, 3)}`)
+
+
+const modules = import.meta.glob('./modules/*.js')
+
+for (const path in modules){
+  modules[path] ().then((module) => {
+    module.load()
+  })
+}
+
 document.querySelector('#app').innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
@@ -18,6 +35,15 @@ document.querySelector('#app').innerHTML = `
       Click on the Vite logo to learn more
     </p>
   </div>
-`
+  <button id="btn">Click!</button>
+  <img id="img" />
+ <pre>${JSON.stringify(data)}</pre>
+ `
+
+document.getElementById('btn').className = buttonStyle.btn
+
+const imagen = document.getElementById('img')
+imagen.src = img
+imagen.className = imagenStyle.img
 
 setupCounter(document.querySelector('#counter'))
